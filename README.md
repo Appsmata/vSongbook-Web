9260cdf7bd5041d8a211146e30d8cf80413d0fb8
Jacksiro <jacksiroke@gmail.com>
2020-03-23 05:54:35 +0300

    descriptions and api modification

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# vSongBookWeb
+# vSongBook Online (Web and API)
+![vSongBook Screenshot](https://github.com/Appsmata/vSongWeb/raw/master/images/screenshot1.png "vSongBook Screenshot 1")
+
+## What it is
+
+_vSongBook_ (Virtual Songbook) is a high quality, visual, open source
+tool to give users access to song lyrics of major worship and praise 
+songbooks used in christian gatherings be it in church, weddings, 
+funerals or anywhere where one feels like singing.
--- as-api/posts-select.php
+++ as-api/posts-select.php
@@ -28,49 +28,26 @@
 			
 			$as_content = as_post_html_fields($song, $userid, $cookieid, $usershtml, null, $htmloptions);
 			
-			if (array_key_exists('when', $as_content)) {
-				$when = @array_key_exists('data', $as_content['when']) ? '<b>'.$as_content['when']['data'] : null;
-				$when .= @array_key_exists('suffix', $as_content['when']) ? ' '.@$as_content['when']['suffix'].'</b>' : null;
-			} else $when = '';
-			
-			if (array_key_exists('where', $as_content)) {
-				$where = @array_key_exists('prefix', $as_content['where']) ? $as_content['where']['prefix'] : null;
-				$where .= @array_key_exists('data', $as_content['where']) ? ' <b>'.@$as_content['where']['data'].'</b>' : null;
-			} else $where = '';
-			
 			if (array_key_exists('who', $as_content)) {
-				$who = @array_key_exists('prefix', $as_content['who']) ? $as_content['who']['prefix'] : null;
-				$who .= @array_key_exists('data', $as_content['who']) ? ' <b>'.@$as_content['who']['data'].'</b>' : null;
-				$who .= @array_key_exists('points', $as_content['who']) ? ' ('. @$as_content['who']['points']['data'].' '. 
-				$as_content['who']['points']['suffix'].')' : null;
+				$who = @array_key_exists('data', $as_content['who']) ? ' '.@$as_content['who']['data'] : null;
 			} else $who = '';
 			
 			array_push($data, array(
 				'postid' 			=> $songid,
-				'basetype' 			=> $song['basetype'],
 				'number' 			=> $song['number'],
 				'title' 			=> $song['title'],
 				'content' 			=> $song['content'],
 				'tags' 				=> array_key_exists('tags', $song) ? $song['tags'] : null,
 				'created' 			=> $song['created'],
 				'categoryid' 		=> $song['categoryid'],
-				'meta_order' 		=> $as_content['meta_order'],
-				'what' 				=> $as_content['what'],
-				'when' 				=> trim($when),
-				'where' 			=> trim($where),
 				'who' 				=> trim($who),
 				'netthumbs' 		=> $song['netthumbs'],
-				'views' 			=> $song['views'],
-				'hotness' 			=> $song['hotness'],
 				'acount' 			=> $song['acount'],
-				'userid' 			=> $song['userid'],
-				'level' 			=> $song['level'],
-				'avatar' 			=> $as_content['avatar'],
-				'thumb_state' 		=> $as_content['thumb_state'])
+				'userid' 			=> $song['userid'])
 			);
 		}
 	}
 
 	$output = json_encode(array('total' => $total, 'data' => $data));
 	
-	echo $output;
+	echo strip_tags($output);
